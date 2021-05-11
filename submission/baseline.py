0c62aaca8ad2d76b929aa706e30f349fbb3f8f85
japierreSWE <japierre@wpi.edu>
2021-05-11 10:26:30 -0400

    Added baseline to submission

---
+++ submission/baseline.py
@@ -0,0 +1,137 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import pickle
+
+def perform_softmax(X, y):
+    # do some randomization on ytr
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+    sm = LogisticRegression()
+    sm.fit(X_train, y_train)
+    yhat = sm.predict_proba(X_test)
+    
+    yhat = yhat[:, 1]
+    
+    score = metrics.roc_auc_score(y_test, yhat, average=None)
+    
+    print(f"\nAUC Score: {score}\n")
+    
+    #metrics.plot_roc_curve(sm, X_test, y_test)
+    #plt.show()
+    f = open('baseline.pckl', 'wb')
+    pickle.dump(sm, f)
+    f.close()
+    print("Model saved")
+    
+    
+def transform_categorical(array):
+    enc = OneHotEncoder(handle_unknown='ignore')
+    enc.fit(array)
+    
+    return enc.transform(array).toarray()
+    
+if __name__ == "__main__":
+    data_path = f'data{os.sep}'
+    train_path = data_path + 'train.csv'
+    test_path = data_path + 'test.csv'
+    
+    dtypes = {
+        'MachineIdentifier':                                    'category',
+        'ProductName':                                          'category',
+        'EngineVersion':                                        'category',
+        'AppVersion':                                           'category',
+        'AvSigVersion':                                         'category',
+        'IsBeta':                                               'int8',
+        'RtpStateBitfield':                                     'float16',
+        'IsSxsPassiveMode':                                     'int8',
+        'DefaultBrowsersIdentifier':                            'float16',
+        'AVProductStatesIdentifier':                            'float32',
+        'AVProductsInstalled':                                  'float16',
+        'AVProductsEnabled':                                    'float16',
+        'HasTpm':                                               'int8',
+        'CountryIdentifier':                                    'int32',
+        'CityIdentifier':                                       'float32',
+        'OrganizationIdentifier':                               'float16',
+        'GeoNameIdentifier':                                    'float32',
+        'LocaleEnglishNameIdentifier':                          'int32',
+        'Platform':                                             'category',
+        'Processor':                                            'category',
+        'OsVer':                                                'category',
+        'OsBuild':                                              'int16',
+        'OsSuite':                                              'int16',
+        'OsPlatformSubRelease':                                 'category',
+        'OsBuildLab':                                           'category',
+        'SkuEdition':                                           'category',
+        'IsProtected':                                          'float16',
+        'AutoSampleOptIn':                                      'int8',
+        'PuaMode':                                              'category',
+        'SMode':                                                'float16',
+        'IeVerIdentifier':                                      'float32',
+        'SmartScreen':                                          'category',
+        'Firewall':                                             'float16',
+        'UacLuaenable':                                         'float64',
+        'Census_MDC2FormFactor':                                'category',
+        'Census_DeviceFamily':                                  'category',
+        'Census_OEMNameIdentifier':                             'float32',
+        'Census_OEMModelIdentifier':                            'float32',
+        'Census_ProcessorCoreCount':                            'float16',
+        'Census_ProcessorManufacturerIdentifier':               'float16',
+        'Census_ProcessorModelIdentifier':                      'float32',
+        'Census_ProcessorClass':                                'category',
+        'Census_PrimaryDiskTotalCapacity':                      'float32',
+        'Census_PrimaryDiskTypeName':                           'category',
+        'Census_SystemVolumeTotalCapacity':                     'float32',
+        'Census_HasOpticalDiskDrive':                           'int8',
+        'Census_TotalPhysicalRAM':                              'float32',
+        'Census_ChassisTypeName':                               'category',
+        'Census_InternalPrimaryDiagonalDisplaySizeInInches':    'float32',
+        'Census_InternalPrimaryDisplayResolutionHorizontal':    'float32',
+        'Census_InternalPrimaryDisplayResolutionVertical':      'float32',
+        'Census_PowerPlatformRoleName':                         'category',
+        'Census_InternalBatteryType':                           'category',
+        'Census_InternalBatteryNumberOfCharges':                'float32',
+        'Census_OSVersion':                                     'category',
+        'Census_OSArchitecture':                                'category',
+        'Census_OSBranch':                                      'category',
+        'Census_OSBuildNumber':                                 'int32',
+        'Census_OSBuildRevision':                               'int32',
+        'Census_OSEdition':                                     'category',
+        'Census_OSSkuName':                                     'category',
+        'Census_OSInstallTypeName':                             'category',
+        'Census_OSInstallLanguageIdentifier':                   'float16',
+        'Census_OSUILocaleIdentifier':                          'int32',
+        'Census_OSWUAutoUpdateOptionsName':                     'category',
+        'Census_IsPortableOperatingSystem':                     'int8',
+        'Census_GenuineStateName':                              'category',
+        'Census_ActivationChannel':                             'category',
+        'Census_IsFlightingInternal':                           'float16',
+        'Census_IsFlightsDisabled':                             'float16',
+        'Census_FlightRing':                                    'category',
+        'Census_ThresholdOptIn':                                'float16',
+        'Census_FirmwareManufacturerIdentifier':                'float32',
+        'Census_FirmwareVersionIdentifier':                     'float32',
+        'Census_IsSecureBootEnabled':                           'int8',
+        'Census_IsWIMBootEnabled':                              'float16',
+        'Census_IsVirtualDevice':                               'float16',
+        'Census_IsTouchEnabled':                                'int8',
+        'Census_IsPenCapable':                                  'int8',
+        'Census_IsAlwaysOnAlwaysConnectedCapable':              'float16',
+        'Wdft_IsGamer':                                         'float16',
+        'Wdft_RegionIdentifier':                                'float32',
+        'HasDetections':                                        'int8'
+    }
+    
+    train_data = pd.read_csv(train_path, nrows=1000)
+    
+    Xtr = train_data[["Firewall", "HasTpm"]].to_numpy()
+    ytr = train_data["HasDetections"].to_numpy()
+    
+    Xtr = np.nan_to_num(Xtr)
+    ytr = np.nan_to_num(ytr)
+    
+    perform_softmax(Xtr, ytr)
